653a244f3356f8603f0abf266f75f1a5f9c2508c
YuriAdonev <zyztem@mail.ru>
2019-03-24 23:42:21 +0700

    Задание 4

--- lesson-1/task-4.js
+++ lesson-1/task-4.js
@@ -3,3 +3,21 @@
 /*
     Задание 4
 */
+
+const promisesArray = [];
+
+for (let i = 0; i < 10; i++) {
+  promisesArray[i] = new Promise((resolve, reject) => {
+    fetch(`https://jsonplaceholder.typicode.com/users/${i+1}`).then((result) => {
+      resolve(result.json());
+    }).catch(() => {
+      reject({});
+    });
+  });
+}
+
+Promise.all(promisesArray).then((result) => {
+  result.forEach((item) => {
+    console.log(item);
+  });
+});
