1a0bce754acabe6b1aed07c29628abc922de0e28
YuriAdonev <zyztem@mail.ru>
2019-03-24 18:00:08 +0700

    Задание 2

--- lesson-1/script.js
+++ lesson-1/script.js
@@ -5,7 +5,7 @@
 */
 
 function loop(times = 0, callback = null) {
-  if (callback != null) {
+  if (callback != null && times > 0) {
     for (let i = 0; i < times; i++) {
       callback();
     }
@@ -15,3 +15,21 @@ function loop(times = 0, callback = null) {
 loop(5, () => {
   console.log('test');
 });
+
+/*
+    Задание 2
+*/
+
+function calculateArea(arg1, arg2) {
+  const area = arg1 * arg2;
+  const figure = (arg1 == arg2) ? 'Квадрат' : 'Прямоугольник';
+  const input = [arg1, arg2];
+  return {
+    area,
+    figure,
+    input
+  }
+}
+
+console.log(calculateArea(10, 10));
+console.log(calculateArea(10, 20));
